708cbf5f9c1f75f2022c8dbd10578f24fdc345ef
Freakshot254 <132798497+Freakshot254@users.noreply.github.com>
2023-05-07 08:47:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repository"
-git clone https://github.com/Greymattersbot/md-renamebot /md-renamebot
-cd /md-renamebot 
+git clone https://github.com/Freakshot254/Chat /Chat
+cd /Chat 
 echo "installing requirements"
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
